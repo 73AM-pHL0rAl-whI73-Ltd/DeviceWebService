02afbcd5f4f6eec29313230a95a3dc95fe4fba9a
Conny Hansson <conhan@hotmail.se>
2021-05-17 13:53:44 +0200

    added project files

---
+++ src/main/java/com/example/devicewebservice/DeviceWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.devicewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devicewebservice/DeviceWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devicewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
