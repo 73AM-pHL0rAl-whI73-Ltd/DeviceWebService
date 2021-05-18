c4df0b8433cabc14b878261f5a48c3ac4b1583f2
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-05-18 11:19:27 +0200

    SocketHandler added

--- src/main/java/com/example/devicewebservice/Models/SocketHandler.java
+++ src/main/java/com/example/devicewebservice/Models/SocketHandler.java
@@ -1,4 +1,34 @@
 package com.example.devicewebservice.Models;
 
-public class SocketHandler {
+import com.google.gson.Gson;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Component
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws InterruptedException, IOException {
+        Map value = new Gson().fromJson(message.getPayload(), Map.class);
+
+        for(WebSocketSession webSocketSession : sessions) {
+            webSocketSession.sendMessage(new TextMessage(""));
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        session.sendMessage(new TextMessage(""));
+    }
 }
--- src/main/java/com/example/devicewebservice/Models/WebSocketConfig.java
+++ src/main/java/com/example/devicewebservice/Models/WebSocketConfig.java
@@ -5,8 +5,6 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import java.util.logging.SocketHandler;
-
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
