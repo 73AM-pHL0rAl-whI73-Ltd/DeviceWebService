d2c68a0b3211f47e0f1ab3e069a3934d8425244f
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-05-18 13:51:26 +0200

    app.js moved to static directory and crossorigin added

--- src/main/java/com/example/devicewebservice/Controllers/WebController.java
+++ src/main/java/com/example/devicewebservice/Controllers/WebController.java
@@ -1,8 +1,10 @@
 package com.example.devicewebservice.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class WebController {
 
