09dc7d4ad42ac9167b9a891038c8e732fe0a2b0d
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-05-19 14:17:02 +0200

    app.js changed for ws

--- src/main/java/com/example/devicewebservice/Controllers/WebController.java
+++ src/main/java/com/example/devicewebservice/Controllers/WebController.java
@@ -3,10 +3,18 @@ package com.example.devicewebservice.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
 public class WebController {
+
+    @RequestMapping(value = "/test", method = GET, headers = "Connection!=Upgrade")
+    public String status() {
+        return "index";
+    }
 
     @GetMapping("/")
     public String index() {
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,16 +1,41 @@
-let tabledata = document.getElementById('tabledata')
+var ws;
+ws = new WebSocket('ws://localhost:8080/test');
+
+ws.onmessage = function (){
+
+    let tabledata = document.getElementById('tabledata')
 
 //Gets data from database from API to print values
-fetch("https://devicewebapi.herokuapp.com/measurements")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        for(let row of data) {
+    fetch("https://devicewebapi.herokuapp.com/measurements")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            for(let row of data) {
 
-            //Converts unixtimestamp to time
-            var unixTimestamp = row.timeStamp;
-            var date = new Date(unixTimestamp*1000);
+                //Converts unixtimestamp to time
+                var unixTimestamp = row.timeStamp;
+                var date = new Date(unixTimestamp*1000);
 
-            //Fills table on htmlpage
-            tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`    }
-    })
+                //Fills table on htmlpage
+                tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`    }
+        })
+}
+
+ws.onopen = function (){
+    let tabledata = document.getElementById('tabledata')
+
+//Gets data from database from API to print values
+    fetch("https://devicewebapi.herokuapp.com/measurements")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            for(let row of data) {
+
+                //Converts unixtimestamp to time
+                var unixTimestamp = row.timeStamp;
+                var date = new Date(unixTimestamp*1000);
+
+                //Fills table on htmlpage
+                tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`    }
+        })
+}
