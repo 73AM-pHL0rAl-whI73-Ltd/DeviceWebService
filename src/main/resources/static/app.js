1fa18959ee5499febd895b3e0ec965fd8070eb8e
Conny Hansson <conhan@hotmail.se>
2021-05-19 14:55:49 +0200

    Added clearing table data

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,41 +1,34 @@
 var ws;
 ws = new WebSocket('ws://localhost:8080/test');
 
+let tabledata = document.getElementById('tabledata');
+
+
+
 ws.onmessage = function (){
-
-    let tabledata = document.getElementById('tabledata')
-
-//Gets data from database from API to print values
-    fetch("https://devicewebapi.herokuapp.com/measurements")
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            for(let row of data) {
-
-                //Converts unixtimestamp to time
-                var unixTimestamp = row.timeStamp;
-                var date = new Date(unixTimestamp*1000);
-
-                //Fills table on htmlpage
-                tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`    }
-        })
+    updateTable();
 }
 
 ws.onopen = function (){
-    let tabledata = document.getElementById('tabledata')
+    updateTable();
+}
 
-//Gets data from database from API to print values
+function updateTable() {
+    //Gets data from database from API to print values
     fetch("https://devicewebapi.herokuapp.com/measurements")
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            for(let row of data) {
+            // clear previous table data
+            tabledata.innerHTML = "";
 
+            for(let row of data) {
                 //Converts unixtimestamp to time
                 var unixTimestamp = row.timeStamp;
                 var date = new Date(unixTimestamp*1000);
 
                 //Fills table on htmlpage
-                tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`    }
+                tabledata.innerHTML += `<tr><td>${row.deviceId}</td><td>${date}</td><td>${row.temperature}</td><td>${row.humidity}</td>`;
+            }
         })
 }
