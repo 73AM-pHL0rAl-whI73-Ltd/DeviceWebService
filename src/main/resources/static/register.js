ac8da8d7ba91e7d7a185ee7cfd4f694c815536be
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-05-21 15:17:52 +0200

    print constring added to register.js

--- src/main/resources/static/register.js
+++ src/main/resources/static/register.js
@@ -1,2 +1,13 @@
 let con = document.getElementById('connection_s');
 let generate = document.getElementById('generate');
+
+function generateCon(){
+    fetch("https://devicewebapi.herokuapp.com/")
+        .then(res => res.json())
+        .then(data => {
+
+                //print on htmlpage
+                con.innerHTML += "";
+                con.innerHTML += {deviceId};
+        })
+}
