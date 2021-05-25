e085da21d3b2a2b7e1145e0b28236920e3d55d30
Linnea Miderkvist <Linnea.miderkvist@gmail.com>
2021-05-25 14:53:47 +0200

    changed alias name and send func name

--- src/main/resources/static/register.js
+++ src/main/resources/static/register.js
@@ -1,11 +1,10 @@
 let con = document.getElementById('connection_s');
-//let generate = document.getElementById('generate');
-let alias = document.getElementById('deviceAlias');
+let inputAlias = document.getElementById('deviceAlias');
 
 // generates a new device from entered device alias and prints device UUID to screen
 function generateCon(){
 
-    fetch(`https://devicewebapi.herokuapp.com/devices/new/${alias.value}`, {method : 'post'})
+    fetch(`https://devicewebapi.herokuapp.com/devices/new/${inputAlias.value}`, {method : 'post'})
         .then(res => res.json())
         .then(data => {
                 // get device UUID
